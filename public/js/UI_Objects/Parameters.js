fe37e077e3dedb00d5fe7a355866186bd0fca4f9
jamiejamiebobamie <jmccrory@vt.edu>
2020-03-27 22:33:47 -0700

    added documentation

--- public/js/UI_Objects/Parameters.js
+++ public/js/UI_Objects/Parameters.js
@@ -1,35 +1,48 @@
-// var someObject = { a: 1, b: 2, c: 3 }
-// var { a: a, b: b, c: c } = someObject
-
+// temporary. prototyping class constructor for ALL ui classes below.
 class Parameters{
-//offset, widthOfParent,heightOfParent, orientationOfScreen,index, lenOfSiblings,func,startingPosition,length
-    constructor({
-        offset= offset,
-        widthOfParent= widthOfParent,
-        heightOfParent= heightOfParent,
-        orientationOfScreen= orientationOfScreen,
-        index= index,
-        lenOfSiblings= lenOfSiblings,
-        func= func,
-        startingPosition= startingPosition,
-        length= length
-        }){
-            this.offset = offset || 0;
-            this.widthOfParent = widthOfParent || windowWidth;
-            this.heightOfParent = heightOfParent || windowHeight;
-            this.portrait = orientationOfScreen || windowWidth < windowHeight
-            this.index = index || 0;
-            this.lenOfSiblings = lenOfSiblings || 1;
-            this.func = func || null;
-            this.startingPosition = startingPosition || 0;
-            if (portrait){
-                this.length = length || windowWidth / this.lenOfSiblings
-            } else {
-                this.length = length || windowHeight / this.lenOfSiblings
-            }
-        }
-        getParameterList(){
-            return "Named Parameters: offset, widthOfParent, heightOfParent, orientationOfScreen, index, lenOfSiblings, func, startingPosition, length"
-        }
+    constructor(parameterObject){
+        // destructuring my parameterObject and intializing tempVariables
+        let {
+            offset: offset,
+            widthOfParent: widthOfParent,
+            heightOfParent: heightOfParent,
+            orientation: orientation,
+            index: index,
+            len: len,
+            func: func,
+            length: length
+            } = parameterObject;
 
+        // setting data members and member functions with the values from tempVariables
+        this.offset = offset || 0;
+        this.widthOfParent = widthOfParent || windowWidth;
+        this.heightOfParent = heightOfParent || windowHeight;
+        this.portrait = orientation || windowWidth < windowHeight;
+        this.index = index || 0;
+        this.len = len || 1;
+        this.func = func || null;
+
+        if (this.portrait){
+            this.startingPosition = startingPosition || this.index * this.widthOfParent / this.len;
+            this.length = length || this.widthOfParent / this.len;
+        } else {
+            this.startingPosition = startingPosition || this.index * this.heightOfParent / this.len;
+            this.length = length || this.heightOfParent / this.len;
+        }
+    }
+
+    // example parameterObject
+    getParameterList(){
+        let parameterObject = {
+            offset: 0,
+            widthOfParent: 200,
+            heightOfParent: 300,
+            orientation: true,
+            index: 0,
+            len: 1,
+            func: null,
+            length: 200
+        };
+        return parameterObject;
+    }
 }
